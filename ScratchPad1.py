990f097c66c788199c0be58495247f30d2030c34
campbellbg <campbell.bg@gmail.com>
2018-09-01 08:38:37 +1000

    Initial Test

---
+++ ScratchPad1.py
@@ -0,0 +1,11 @@
+
+def getDomain(inEmail):
+    return inEmail[inEmail.find('@') + 1:]
+
+#Run this block when this file is executed direct i.e. as a the main call
+
+if __name__ == '__main__':
+
+    theEmail = 'campbell.bggmail.com'
+
+    print(getDomain(theEmail))
