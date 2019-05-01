e7277e13696e51f1fd5cbfa977eff09d9cef5828
campbellbg <campbell.bg@gmail.com>
2019-05-01 19:46:06 +1000

    Finished writing code for exercise. Compare with notebook tomorrow am

--- DecisionRandomForests.py
+++ DecisionRandomForests.py
@@ -18,21 +18,49 @@ np.set_printoptions(linewidth=400)
 #['not.fully.paid']
 dataLoc = 'D:\\Course Content\\python-for-data-science-and-ml-bootcamp\\15-Decision-Trees-and-Random-Forests\\'
 
-loans = pd.read_csv(dataLoc + 'loan_data.csv')
+loans_orig = pd.read_csv(dataLoc + 'loan_data.csv')
 
 #Look at the completeness of the data
+#sb.heatmap(data=loans.isna())
+#plt.show()
+
+#Do we have a balanced classifier => Not really. 8045 Paid, 1533 Not Fully Paid.
+#print(loans['not.fully.paid'].value_counts())
+
+#This block of code way playing around with balancing the classifiers
+#loans = loans_orig
+loans = pd.concat([loans_orig[loans_orig['not.fully.paid'] == 0].loc[1:2000], loans_orig[loans_orig['not.fully.paid'] == 1]], axis=0)
+loans.reset_index()
 
 #Look at the categorical variables. One-Hot Encode these perhaps
 
-#Remove features that aren't really useful i.e. not very predictive ....
+#purpose => debt_consolidation, all_other, credit_card, home_improvement, small_business, major_purchase, educational
+#print(loans['purpose'].value_counts())
+#print(loans[['purpose', 'int.rate']].groupby('purpose').count())
+
+#The independent variables dataframe
+loans_dep = pd.concat([loans[['credit.policy', 'int.rate', 'installment', 'log.annual.inc', 'dti', 'fico', 'days.with.cr.line', 'revol.bal', 'revol.util', 'inq.last.6mths', 'delinq.2yrs', 'pub.rec']], pd.get_dummies(data=loans['purpose'], drop_first=True)], axis=1)
 
 #train test split
-
-#Random Forest
-
-#Confusion Matrix and Classification Report
+loans_x_train, loans_x_test, loans_y_train, loans_y_test = train_test_split(loans_dep, loans['not.fully.paid'], test_size=0.3, random_state=101)
 
 '''
+#Single decision tree. Capture performance for comparison with the Random Forest
+my_dtree = DecisionTreeClassifier().fit(X=loans_x_train, y=loans_y_train)
+loan_predictions = my_dtree.predict(X=loans_x_test)
+
+'''
+
+#Random Forest
+my_rftree = RandomForestClassifier(n_estimators=500).fit(X=loans_x_train, y=loans_y_train)
+loan_predictions = my_rftree.predict(X=loans_x_test)
+
+#Confusion Matrix and Classification Report
+print(confusion_matrix(y_true=loans_y_test, y_pred=loan_predictions))
+print(classification_report(y_true=loans_y_test, y_pred=loan_predictions))
+
+'''**********************************************************************************************************************
+
 #['Kyphosis', 'Age', 'Number', 'Start']
 my_df = pd.read_csv(dataLoc + 'kyphosis.csv')
 
